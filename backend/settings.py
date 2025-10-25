cf6d0eee6617db946aa90414dcea5faf72191fb8
Mrigank22 <p.mrigank22@gmail.com>
2025-10-25 17:27:32 +0530

    added MEDIA_ROOT,STATIC_ROOT

--- backend/settings.py
+++ backend/settings.py
@@ -181,7 +181,9 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
