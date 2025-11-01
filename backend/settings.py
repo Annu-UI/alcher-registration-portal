225be74a9973950c1ae89e5f513208492fe65e58
Amaan Farooq <khan.amaanklz@gmail.com>
2025-11-02 01:43:34 +0530

    fixed tokken issue

--- backend/settings.py
+++ backend/settings.py
@@ -26,6 +26,8 @@ SESSION_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_DOMAIN = '.alcheringa.co.in' 
+CSRF_COOKIE_DOMAIN = '.alcheringa.co.in'
 PROD = os.environ.get('PROD',False)
 
 if os.environ.get('PROD'):
